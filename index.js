cd2b776c8c1517ed64c5bccef9e0a2c6cc1c7a45
Muhammad Ahsan Naseer <manaseer@gmail.com>
2019-07-15 10:20:42 -0400

    remove mongodb credentials

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://cms:cms123@ds245548.mlab.com:45548/cms',{useNewUrlParser: true});
 
 //bodyParser setup
 
